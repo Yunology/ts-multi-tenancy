12f62c9ef40f38edc2524f539846ce925ef6231f
Clooooode <jackey8616@gmail.com>
2022-11-14 10:22:43 +0800

    fix: missing export

---
+++ src/controller/index.ts
@@ -0,0 +1,2 @@
+// src/controller/index.ts
+export * from './tenant';
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 // src/index.ts
-import { TenantService } from '@/service';
+import { TenantService } from './service';
 
 let tenantService: TenantService;
 
+export * from './controller';
+export * from './dto';
+export * from './entry';
+export * from './infrastructure';
+export * from './service';
+export * from './datasource';
 export function getTenantService(): TenantService {
   return tenantService;
 }
