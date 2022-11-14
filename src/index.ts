481b4cb2d2764d1e854cb69d71aa57249e3638d1
Clooooode <jackey8616@gmail.com>
2022-11-14 20:31:16 +0800

    feat: add initMultiTenancy function export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 // src/index.ts
-import { TenantService } from './service';
+import { EntityManager } from 'typeorm';
+
+import { Service, TenantService } from './service';
+import {
+  initRedisDataSource, initSessionRedisStore, getSystemDataSource,
+} from './datasource';
+import {
+  DatabaseInfrastructure, TenantInfrastructure,
+} from './infrastructure';
+import { TenantPlanInfo } from './entry';
 
 let tenantService: TenantService;
 
@@ -10,6 +19,36 @@ export * from './entry';
 export * from './infrastructure';
 export * from './service';
 export * from './datasource';
+
+export async function initMultiTenancy(
+  loadPlanCallback: () => Promise<Record<string, TenantPlanInfo>>,
+  initInfrastructureCallback: () =>  void,
+  initModuleCallback: () => Promise<Record<string, Service>>,
+  preCreateSystemDatasFunction?: (manager: EntityManager) => Promise<void>,
+  preCreateTenantDatasFunction?: () => Promise<void>,
+): Promise<void> {
+  tenantService = new TenantService();
+  const redisDataSource = await initRedisDataSource();
+  const sessionStore = await initSessionRedisStore();
+  const systemDataSource = await getSystemDataSource().initialize();
+  await systemDataSource.transaction('SERIALIZABLE', async (manager: EntityManager) => {
+    await DatabaseInfrastructure.init();
+    await TenantInfrastructure.init();
+    await tenantService.loadPlans(loadPlanCallback);
+    await tenantService.initInfrastructures(initInfrastructureCallback);
+    await tenantService.initModules(initModuleCallback);
+    if (preCreateSystemDatasFunction !== undefined) {
+      await preCreateSystemDatasFunction(manager);
+    }
+    await tenantService.initDatabases(manager);
+    await tenantService.precreateTenantries(manager);
+    if (preCreateTenantDatasFunction !== undefined) {
+      await preCreateTenantDatasFunction();
+    }
+    await tenantService.initlializeTenantries();
+  });
+}
+
 export function getTenantService(): TenantService {
   return tenantService;
 }
