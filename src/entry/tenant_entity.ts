6d2758849387d7698d26fb1bca3d8b700096c8b4
Clooooode <jackey8616@gmail.com>
2022-11-24 23:03:40 +0800

    chore: change TenantEntity fields with uuid

--- src/entry/tenant_entity.ts
+++ src/entry/tenant_entity.ts
@@ -1,11 +1,9 @@
 // src/entry/tenant_entity.ts
-import { JoinColumn, ManyToOne } from 'typeorm';
+import { Column } from 'typeorm';
 
 import { BaseEntity } from './base_entity';
-import { Tenant } from './tenant.entry';
 
 export class TenantEntity extends BaseEntity {
-  @ManyToOne(() => Tenant, (t) => t.id)
-  @JoinColumn({ name: 'tenant' })
-  tenant!: Tenant;
+  @Column({ name: 'tenant_id', type: 'uuid' })
+  tenantId!: string;
 }
