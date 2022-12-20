f279e2d29aa6ee3748fb88f3795ffdc4b3797b35
Clooooode <jackey8616@users.noreply.github.com>
2022-12-20 20:03:26 +0800

    Feat/service config & permission generic type (#13)

--- src/service/data_service.ts
+++ src/service/data_service.ts
@@ -1,9 +1,12 @@
 // src/service/data_service.ts
 import { EntityManager } from 'typeorm';
 
-import { Service } from './service';
+import { ConfigTree, PermissionTree, Service } from './service';
 
-export abstract class DataService extends Service {
+export abstract class DataService<
+  P extends PermissionTree = {},
+  C extends ConfigTree = {},
+> extends Service<P, C> {
   serialTran<T>(
     runInTransaction: (manager: EntityManager) => Promise<T>,
   ): Promise<T> {
