a07bc9597cd8860630c04ead053e6509a0bdc056
Clooooode <jackey8616@gmail.com>
2023-01-19 22:07:28 +0800

    fix: system migration show command

--- src/cli_command/system_migration_show.ts
+++ src/cli_command/system_migration_show.ts
@@ -4,9 +4,11 @@ import { exec } from 'shelljs';
 import { cwd } from 'process';
 
 export class SystemMigrtionRunCommand implements CommandModule {
+  moduleName = 'node_modules/@yunology/ts-multi-tenancy';
   command = 'migration:system show';
+  describe = 'Show system migration';
 
   async handler(args: Arguments) {
-    exec(`yarn typeorm migration:show -d $${cwd()}/node_modules/ts-multi-tenancy/dis/cli_datasource.ts`);
+    exec(`yarn typeorm migration:show -d ${cwd()}/${this.moduleName}/dist/cli_datasource.js`);
   }
 }
