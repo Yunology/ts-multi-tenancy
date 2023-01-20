e45d145248890a77c3eddebf898146319fb1c4ab
Clooooode <jackey8616@gmail.com>
2023-01-20 16:48:03 +0800

    refactor: shelljs to child_process

--- src/cli_command/system_migration_show.ts
+++ src/cli_command/system_migration_show.ts
@@ -1,7 +1,7 @@
 // src/cli_command/system_migration_show.ts
 import { Arguments, CommandModule } from 'yargs';
-import { exec } from 'shelljs';
 import { cwd } from 'process';
+import { spawn } from 'child_process';
 
 export class SystemMigrationShowCommand implements CommandModule {
   command = 'show';
@@ -9,8 +9,15 @@ export class SystemMigrationShowCommand implements CommandModule {
 
   async handler(_: Arguments) {
     const moduleName = 'node_modules/@yunology/ts-multi-tenancy';
-    exec(
-      `yarn typeorm migration:show -d ${cwd()}/${moduleName}/dist/cli_datasource.js`,
+    spawn(
+      'yarn',
+      [
+        'typeorm',
+        'migration:show',
+        '-d',
+        `${cwd()}/${moduleName}/dist/cli_datasource.js`,
+      ],
+      {stdio: 'inherit'},
     );
   }
 }
