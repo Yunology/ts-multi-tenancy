fdc44a45182ece1f96c62f7433bd3f9007e5beab
Clooooode <jackey8616@gmail.com>
2023-01-21 14:58:34 +0800

    chore: remove pkg express-session
    Since we don't need it anymore.

--- src/types/express-session/index.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/express-session/index.d.ts
-import * as expressSession from 'express-session';
-
-declare module 'express-session' {
-  interface SessionData {
-    user: User | undefined;
-  }
-}
