5da8a3cc662450a01dd1a70fd172931d9cbc7f40
Clooooode <jackey8616@gmail.com>
2023-01-21 14:57:49 +0800

    fix: actual remove the lines

--- src/datasource.ts
+++ src/datasource.ts
@@ -2,8 +2,6 @@
 import { DataSource, LoggerOptions } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { createClient, RedisClientType } from 'redis';
-// import session from 'express-session';
-// import connectRedis, { RedisStore } from 'connect-redis';
 
 // Tenant entitires & migrations
 import { Tenant, Database } from './entry';
@@ -12,12 +10,8 @@ import {
   BaseEntityIdField1668675504073,
 } from './migration';
 
-// const RedisStore = connectRedis(session);
-
 const connectionPools: Record<string, DataSource> = {};
 let redisDataSource: RedisClientType;
-// let sessionStore: RedisStore;
-// let sessionRedis: RedisClientType;
 
 export function createDataSource(
   dbName: string,
@@ -80,37 +74,14 @@ export function createRedisDataSource(url: string): RedisClientType {
   redisDataSource = createClient({ url });
   return redisDataSource;
 }
-/*
-export function createSessionRedisStore(url: string): RedisClientType {
-  if (sessionRedis !== undefined) {
-    throw new Error('SessionRedis is already created.');
-  }
 
-  sessionRedis = createClient({ url, legacyMode: true });
-  sessionStore = new RedisStore({ client: sessionRedis });
-  return sessionRedis;
-}
-*/
 export async function initRedisDataSource(): Promise<RedisClientType> {
   if (redisDataSource !== undefined) {
     await redisDataSource.connect();
   }
   return redisDataSource;
 }
-/*
-export async function initSessionRedisStore(): Promise<RedisStore> {
-  if (sessionRedis !== undefined) {
-    await sessionRedis.connect();
-  }
-  return sessionStore;
-}
-*/
+
 export function getRedisDataSource(): RedisClientType {
   return redisDataSource;
 }
-
-/*
-export function getSessionRedisStore(): RedisStore {
-  return sessionStore;
-}
-*/
