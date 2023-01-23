246d8fccf025922d2df814e8cb40b51c56e22748
Clooooode <jackey8616@gmail.com>
2023-01-24 03:15:40 +0800

    test: apply to config decorator

---
+++ test/unit/helper/config.spec.ts
@@ -0,0 +1,19 @@
+// test/unit/helper/config.spec.ts
+import { expect } from 'chai';
+
+import { SetupDefaultConfig } from 'helper';
+import { ConfigTree } from 'index';
+
+describe('Helper Config', () => {
+  describe('Decorator SetupDefaultConfig', () => {
+    it('Should raise error if class is not extended Service', async () => {
+      interface NotExtendedServiceConfig extends ConfigTree {}
+
+      class NotExtendedServiceClass {}
+
+      expect(() => SetupDefaultConfig<NotExtendedServiceConfig>({})(NotExtendedServiceClass)).to.throw(
+        `SetupDefaultConfig can only use at Service's child classes.`,
+      );
+    });
+  });
+});
